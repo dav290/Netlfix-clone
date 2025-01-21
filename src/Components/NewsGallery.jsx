f683badbb6e35028907e9e84ccf47cc4db9884bb
dav290 <164292382+dav290@users.noreply.github.com>
2025-01-22 02:49:59 +0300

    Update NewsGallery.jsx

--- src/Components/NewsGallery.jsx
+++ src/Components/NewsGallery.jsx
@@ -1,23 +1,30 @@
 import React, { useState } from 'react'
-import { Infos } from '../dummy'
+import { Infos } from '../dummy' // Importing dummy data from 'dummy.js' file
+
 function NewsGallery() {
-    const [data,setData]=useState(Infos)
-  return (
-    <div className='w-full'>
-    <div className='w-full flex items-center justify-start py-2'>
-    <h1 className='text-white font-bold ml-3'>New and Popular</h1>
-    </div>
-    <div className='w-full flex items-center justify-start gap-1 ml-3'>
-     {data.slice(15,20).map((item)=>{
-        return(
-            <div key={item.id} className='w-64 h-auto flex-shrink-0'>
-            <img src={item.primaryImage} alt='Movies' className='w-64 h-auto object-contain'/>
+    // Using state to hold and manage the data
+    const [data, setData] = useState(Infos)
+
+    return (
+        <div className='w-full'>
+            {/* Section Title for New and Popular */}
+            <div className='w-full flex items-center justify-start py-2'>
+                <h1 className='text-white font-bold ml-3'>New and Popular</h1> {/* Displaying the section title */}
             </div>
-        )
-     })}
-    </div>
-    </div>
-  )
+
+            {/* Image Gallery */}
+            <div className='w-full flex items-center justify-start gap-1 ml-3'>
+                {/* Mapping through the 'data' array and displaying images */}
+                {data.slice(15, 20).map((item) => {
+                    return (
+                        <div key={item.id} className='w-64 h-auto flex-shrink-0'>
+                            <img src={item.primaryImage} alt='Movies' className='w-64 h-auto object-contain' /> {/* Displaying movie image */}
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
 
 export default NewsGallery
