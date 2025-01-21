165cbaec04059af1f7764a2684b9c31dbb7ab484
dav290 <164292382+dav290@users.noreply.github.com>
2025-01-22 02:48:45 +0300

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,50 +1,54 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom' // Importing NavLink from react-router-dom to manage navigation
+
 function Navbar() {
-    const linkClasses='text-white text-lg'
-    const activeLinkClasses='font-bold'
-  return (
-    <div className='w-full bg-black h-1/4 flex items-center justify-between py-3 bg-opacity-50 z-10'>
-   <div className='w-1/4 text-center'>
-   <h1 className='text-red-700 text-3xl'>Netflix</h1>
-   </div>
-<div className='w-2/4 h-full items-center  flex justify-around'>
- <NavLink to='/'
- className={({isActive})=> `${linkClasses} ${isActive? activeLinkClasses:""}`}
- 
- >
-  Home
+    // Defining common classes for the links and the active link
+    const linkClasses = 'text-white text-lg'  // Base link style
+    const activeLinkClasses = 'font-bold'    // Style for the active link (bold)
 
- </NavLink>
-  
- <NavLink to='/TvShows'
- className={({isActive})=> `${linkClasses} ${isActive? activeLinkClasses:""}`}
- 
- >
-  TvShows
+    return (
+        <div className='w-full bg-black h-1/4 flex items-center justify-between py-3 bg-opacity-50 z-10'>
+            {/* Navbar Title */}
+            <div className='w-1/4 text-center'>
+                <h1 className='text-red-700 text-3xl'>Netflix</h1> {/* Displaying the Netflix title */}
+            </div>
 
- </NavLink>
- <NavLink to='/Movies'
- className={({isActive})=> `${linkClasses} ${isActive? activeLinkClasses:""}`}
- 
- >
-  Movies
+            {/* Navbar Links */}
+            <div className='w-2/4 h-full items-center flex justify-around'>
+                {/* Navigation Link for Home */}
+                <NavLink 
+                    to='/' 
+                    className={({ isActive }) => `${linkClasses} ${isActive ? activeLinkClasses : ""}`} 
+                >
+                    Home
+                </NavLink>
 
- </NavLink>
- <NavLink to='/News'
- className={({isActive})=> `${linkClasses} ${isActive? activeLinkClasses:""}`}
- 
- >
-  New and Popular
+                {/* Navigation Link for TV Shows */}
+                <NavLink 
+                    to='/TvShows' 
+                    className={({ isActive }) => `${linkClasses} ${isActive ? activeLinkClasses : ""}`}
+                >
+                    TvShows
+                </NavLink>
 
- </NavLink>
-</div>
-   
+                {/* Navigation Link for Movies */}
+                <NavLink 
+                    to='/Movies' 
+                    className={({ isActive }) => `${linkClasses} ${isActive ? activeLinkClasses : ""}`}
+                >
+                    Movies
+                </NavLink>
 
-
-
-    </div>
-  )
+                {/* Navigation Link for New and Popular */}
+                <NavLink 
+                    to='/News' 
+                    className={({ isActive }) => `${linkClasses} ${isActive ? activeLinkClasses : ""}`}
+                >
+                    New and Popular
+                </NavLink>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
